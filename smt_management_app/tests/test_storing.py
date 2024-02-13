6bd713bb83ed8407fb2d6c9a253a3ca19a4b1e97
Luca Ballenthin <LB@atn.ad>
2024-02-13 19:23:05 +0100

    extend storing tests

--- smt_management_app/tests/test_storing.py
+++ smt_management_app/tests/test_storing.py
@@ -17,73 +17,65 @@ class StoringTestCase(TestCase):
         print("Running tests for storing.")
         self.client = Client()
 
-        self.article_names = [f"article_{i}" for i in range(1, 10)]
-        for article_name in self.article_names:
-            Article.objects.create(name=article_name)
+        self.article_name = "article_0"
+        Article.objects.create(name=self.article_name)
 
-        self.carrier_names = [f"carrier_{i}" for i in range(1, 10)]
-        for i, carrier_name in enumerate(self.carrier_names):
-            Carrier.objects.create(
-                name=carrier_name,
-                article=Article(self.article_names[i]),
-                delivered=True,
-            )
+        self.carrier_name = "carrier_0"
+        Carrier.objects.create(
+            name=self.carrier_name,
+            article=Article(self.article_name),
+            delivered=True,
+        )
 
-        self.storage_names = [f"storage_{i}" for i in range(1, 3)]
-        for storage_name in self.storage_names:
-            Storage.objects.create(name=storage_name, device="Dummy", capacity=1400)
+        self.storage_name = "storage_0"
+        Storage.objects.create(name=self.storage_name, device="Dummy", capacity=1400)
 
         self.storage_slot_names = [i for i in range(1, 1401)]
         for storage_slot_name in self.storage_slot_names:
             StorageSlot.objects.create(
                 name=storage_slot_name,
-                qr_value=f"{self.storage_names[0]}_{storage_slot_name}_qr_value",
-                storage=Storage(self.storage_names[0]),
-            )
-            StorageSlot.objects.create(
-                name=storage_slot_name,
-                qr_value=f"{self.storage_names[1]}_{storage_slot_name}_qr_value",
-                storage=Storage(self.storage_names[1]),
+                qr_value=f"{self.storage_name}_{storage_slot_name}_qr_value",
+                storage=Storage(self.storage_name),
             )
 
     def test_workinglight_on(self):
-        query = input(f"Are all workinglights of {self.storage_names} green ? [Y/n]")
+        query = input(f"Are all workinglights of {self.storage_name} green ? [Y/n]")
         if query.lower() == "n":
             raise Exception
 
     def test_store_carrier_success(self):
         # user does everything right
-        for i, carrier_name in enumerate(self.carrier_names):
-            storage_name = self.storage_names[(i % 2) - 1]
-            response_store = self.client.get(
-                f"/api/store_carrier/{carrier_name}/{storage_name}/"
-            )
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
 
-            if not response_store.json()["success"]:
-                raise Exception
-            slot_name = response_store.json()["slot"]
-            query = input(
-                f"Is slot {slot_name} of storage {storage_name} blue and is the workinglight yellow? [Y/n]"
-            )
-            if query.lower() == "n":
-                raise Exception
+        if not response_store.json()["success"]:
+            raise Exception
+        slot_name = response_store.json()["slot"]
+        query = input(
+            f"Is slot {slot_name} of storage {storage_name} blue and is the workinglight yellow? [Y/n]"
+        )
+        if query.lower() == "n":
+            raise Exception
 
-            response_confirm = self.client.get(
-                f"/api/store_carrier_confirm/{carrier_name}/{storage_name}/{slot_name}/"
-            )
-            if not response_confirm.json()["success"]:
-                raise Exception
+        response_confirm = self.client.get(
+            f"/api/store_carrier_confirm/{carrier_name}/{storage_name}/{slot_name}/"
+        )
+        if not response_confirm.json()["success"]:
+            raise Exception
 
-            query = input(
-                f"Is slot {slot_name} of storage {storage_name} turned off and is the workinglight back to only green ? [Y/n]"
-            )
-            if query.lower() == "n":
-                raise Exception
+        query = input(
+            f"Is slot {slot_name} of storage {storage_name} turned off and is the workinglight back to only green ? [Y/n]"
+        )
+        if query.lower() == "n":
+            raise Exception
 
     def test_store_carrier_fail_wrong_slot(self):
         # user scans wrong slot
-        carrier_name = self.carrier_names[0]
-        storage_name = self.storage_names[0]
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
 
         response_store = self.client.get(
             f"/api/store_carrier/{carrier_name}/{storage_name}/"
@@ -111,6 +103,100 @@ class StoringTestCase(TestCase):
             f"Is slot {false_slot_name} of storage {storage_name} red for a short duration? [Y/n]"
         )
         if query.lower() == "n":
+            raise Exception
+
+    def test_store_carrier_fail_carrier_does_not_exists(self):
+        carrier_name = "carrier_that_doesn_not_exsist"
+        storage_name = self.storage_name
+
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store.json()["success"]:
+            raise Exception
+
+    def test_store_carrier_fail_carrier_is_collecting(self):
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        response_confirm = self.client.get(
+            f"/api/store_carrier_confirm/{carrier_name}/{storage_name}/{response_store.json()['slot']}/"
+        )
+        response_collecting = self.client.patch(f"/api/collect_carrier/{carrier_name}/")
+        response_store_again = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store_again.json()["success"]:
+            raise Exception
+
+    def test_store_carrier_fail_carrier_is_archived(self):
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_archive = self.client.patch(
+            f"/api/carrier/{carrier_name}/", json={"archived": True}
+        )
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store.json()["success"]:
+            pp(response_archive.json())
+            raise Exception
+
+    def test_store_carrier_fail_is_not_delivered(self):
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_archive = self.client.patch(
+            f"/api/carrier/{carrier_name}/", {"delivered": False}
+        )
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store.json()["success"]:
+            raise Exception
+
+    def test_store_carrier_fail_is_stored(self):
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        slot_name = response_store.json()["slot"]
+        response_confirm = self.client.get(
+            f"/api/store_carrier_confirm/{carrier_name}/{storage_name}/{slot_name}/"
+        )
+        response_store_again = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store_again.json()["success"]:
+            raise Exception
+
+    def test_store_carrier_fail_storage_does_not_exists(self):
+        carrier_name = self.carrier_name
+        storage_name = "stroage_that_does_not_exsist"
+
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+        if response_store.json()["success"]:
+            raise Exception
+
+    def test_store_carrier_fail_no_free_slot(self):
+        # create Carrier for every slot and fill the slot
+        article = Article.objects.get(name=self.article_name)
+        for slot_name in self.storage_slot_names:
+            slot = StorageSlot.objects.get(name=slot_name)
+            Carrier.objects.create(
+                name=f"carrier_{slot_name}", storage_slot=slot, article=article
+            )
+        carrier_name = self.carrier_name
+        storage_name = self.storage_name
+        response_store = self.client.get(
+            f"/api/store_carrier/{carrier_name}/{storage_name}/"
+        )
+
+        if response_store.json()["success"]:
             raise Exception
 
     def test_store_carrier_choose_slot_flow(self):
