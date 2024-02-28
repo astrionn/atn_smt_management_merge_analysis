3923af6d5557ed0646a8d03496a7106448bcc96e
Luca Ballenthin <LB@atn.ad>
2024-02-28 12:39:21 +0100

    test script/ps1 creates some random carriers, added DEMO script, added script to patch in carrier names

--- smt_management_app/scripts/import_ATN_storage_demo.py
+++ smt_management_app/scripts/import_ATN_storage_demo.py
@@ -1,28 +1,42 @@
-from smt_management_app.models import *
+from ipaddress import ip_address
+from smt_management_app.models import (
+    Manufacturer,
+    Provider,
+    Storage,
+    StorageSlot,
+    Article,
+    Carrier,
+)
 import random
 
 
 def run():
-    storage = Storage.objects.create(name=f"Storage_1", capacity=1400, device="Dummy")
-    storage2 = Storage.objects.create(name=f"Storage_2", capacity=1400, device="Dummy")
+    storage = Storage.objects.create(
+        name=f"Storage_1",
+        capacity=1400,
+        device="NeoLight",
+        ip_address="192.168.178.12",
+        ip_port=5000,
+    )
     for i in range(1, 1401):
         storage_slot = StorageSlot.objects.create(name=i, storage=storage)
-        storage_slot2 = StorageSlot.objects.create(name=i, storage=storage2)
-        if i > 700:
+
+        if storage_slot.name > 700:
             i -= 700
+
+        if (
+            storage_slot.name > 600 and storage_slot.name <= 700
+        ):  # top row was inserted the wrong way around but the qr codes are not
+            i = 1301 - i
 
         row = (i - 1) // 100 + 1
         lamp = (i - 1) % 100 + 1
-        if int(storage_slot.name) > 700:
+        if storage_slot.name > 700:
             lamp += 100
-
-        qr_val = f"001-{str(row).zfill(2)}-{str(lamp).zfill(3)}"
+        qr_val = f"002-{str(row).zfill(2)}-{str(lamp).zfill(3)}"
 
         storage_slot.qr_value = qr_val
         storage_slot.save()
-
-        storage_slot2.qr_value = qr_val
-        storage_slot2.save()
 
     manufacturers = []
     for i in range(1, 4):
