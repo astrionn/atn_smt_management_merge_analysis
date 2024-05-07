53b769e2103ffa8d99daa8ad6faebf3edb3ac1f6
Ingo Otjen <ingo.o@weytronik.local>
2024-05-07 12:42:46 +0200

    modify setup scripts for storage2

--- smt_management_app/scripts/weytronik_storage_setup.py
+++ smt_management_app/scripts/weytronik_storage_setup.py
@@ -2,15 +2,12 @@ from smt_management_app.models import Storage, StorageSlot
 
 
 def run():
-    storage = Storage.objects.create(
-        name="Storage_1",
-        capacity=1400,
-        device="NeoLight",
-        ip_address="192.168.178.11",
-        ip_port=5000,
+    storage = Storage.objects.get(
+        name="Storage_2",
     )
 
     for i, j in enumerate(range(1, storage.capacity + 1)):
+        i+=1
         if i > 700:
             i -= 700
 
@@ -18,5 +15,7 @@ def run():
         lamp = (i - 1) % 100 + 1
         if j > 700:
             lamp += 100
-        qr_value = f"001-{str(row).zfill(2)}-{str(lamp).zfill(3)}"
+        # modify head of following line for storage 1/2/3...
+        qr_value = f"002-{str(row).zfill(2)}-{str(lamp).zfill(3)}"
         slot = StorageSlot.objects.create(name=j, storage=storage, qr_value=qr_value)
+        #print(f"{i=} || {j=} || {row=} || {lamp=} || {qr_value=} " )
